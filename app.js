71b4d0c38d1f44ad7a0e12b6edde264496ccec45
Subhani-syed <subhani6352@gmail.com>
2022-01-23 12:38:59 +0530

    Update procfile

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const https = require('https');
 const path =require('path');
 
 const app = express();
-const PORT = 3000||process.env.PORT;
+const PORT = process.env.PORT||3000;
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
